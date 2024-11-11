4625f113c0eded3fefdc44253e4f61b8a05eaef1
Felipe Alzamora <luisfalmi@gmail.com>
2024-11-11 08:22:44 -0300

    add some changes styles in components

---
+++ src/pages/Products.jsx
@@ -0,0 +1,14 @@
+const Productos = () => {
+    return (
+        <div className="flex flex-col">
+
+            <div className="flex-1 flex flex-col gap-32 px-32 py-16">
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Productos;
