c0e7bfaf4a0bf444c401ecb7858009628da93e63
Franco <fr.cabanillas7@gmail.com>
2024-11-19 18:55:22 -0300

    navbar fijado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Venta Gamer
