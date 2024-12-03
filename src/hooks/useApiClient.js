ccfa70ff8b503db909697b11c856a606130b6f93
Lucas Alderete <lucasm.alderete@gmail.com>
2024-12-02 23:15:09 -0300

    fix baseUrl /api/

--- src/hooks/useApiClient.js
+++ src/hooks/useApiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const useApiClient = () => {
 
   const apiClient = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://localhost:8080/api",
     headers: {
       "Content-Type": "application/json",
     },
